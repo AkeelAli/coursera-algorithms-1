6982a9ae7ae28b832fc70350fac759a78938d328
Akeel <agilowen@gmail.com>
2013-07-23 16:03:29 +0000

    completed min_cut with wrapper script

--- programming_question_3/min_cut.py
+++ programming_question_3/min_cut.py
@@ -1,7 +1,15 @@
+import random
+
 class Edge:
 	def __init__(self, id1, id2):
 		self.id1 = id1
 		self.id2 = id2
+
+        def set_id1(self,id1):
+            self.id1 = id1
+
+        def set_id2(self,id2):
+            self.id2 = id2
 	
 	def __str__(self):
 		return "(%d,%d)" % (self.id1, self.id2)
@@ -9,7 +17,7 @@ class Edge:
 #complex vertex
 class CVertex:
 	def __init__(self, id):
-		self.size = 0
+		self.size = 1
 		self.vertices = []
 		self.add_vertex(id)
 
@@ -24,12 +32,28 @@ class CVertex:
 		self.edges.append(edge)
 		self.num_edges +=1
 
+        def remove_edge(self, edge):
+                if edge in self.edges:
+                    self.edges.remove(edge)
+                    self.num_edges -= 1
+                else:
+                    print "ERROR: cannot find edge (%d,%d) in CVertex %d" % (edge.id1, edge.id2, self.get_id())
+
+
 	def get_id(self):
 		return self.vertices[0]
 	
 	def __str__(self):
-		str = ""
-		for 
+		string = "%d\t" % self.get_id()
+                for edge in self.edges:
+                    if edge.id1 == self.get_id():
+                        string = string + str(edge.id2) + "\t"
+                    else:
+                        string = string + str(edge.id1) + "\t"
+                return string
+                    
+                    
+		 
 
 class Graph:
 	def __init__(self):
@@ -42,9 +66,25 @@ class Graph:
 		self.edges.append(edge)
 		self.num_edges += 1
 
+        def remove_edge(self, edge):
+                if edge in self.edges:
+                    self.edges.remove(edge)
+                    self.num_edges -= 1
+                else:
+                    print "ERROR: cannot find edge (%d,%d) in graph" % (edge.id1, edge.id2)
+
+                
+
 	def add_cVertex(self, cVertex):
 		self.cVertices.append(cVertex)
 		self.num_vertices += 1
+
+        def remove_cVertex(self, cVertex):
+                if cVertex in self.cVertices:
+                    self.cVertices.remove(cVertex)
+                    self.num_vertices -= 1
+                else:
+                    print "ERROR: cannot find vertex %d in graph" % cVertex.get_id()
 
 	def is_edge_in_graph(self, in_edge):
 		for edge in self.edges:
@@ -54,8 +94,58 @@ class Graph:
 				return True
 
 		return False
+
+        def get_cVertex(self, vertex_id):
+            for vertex in self.cVertices:
+                if vertex.get_id() == vertex_id:
+                    return vertex
+
+            print "ERROR: could not find vertex %d" % vertex_id
+            return None
+
+        def random_contract(self):
+            edge_index = random.randint(0, self.num_edges - 1)
+
+            ran_edge = self.edges[edge_index]
+            id1 = ran_edge.id1
+            id2 = ran_edge.id2
+
+ #           print "...contracting edge (%d,%d)" % (id1, id2)
+
+            cVertex1 = self.get_cVertex(id1)
+            cVertex2 = self.get_cVertex(id2)
+
+            fused_cVertex = CVertex(id1)
+            fused_cVertex.add_vertex(id2)
+
+            self.remove_cVertex(cVertex1)
+            self.remove_cVertex(cVertex2)
+
+            self.add_cVertex(fused_cVertex)
+
+            #change edge id2 to id1 in all references
+            to_remove = []
+            for edge in self.edges:
+                if edge.id1 == id2:
+                    edge.set_id1(id1)
+                if edge.id2 == id2:
+                    edge.set_id2(id1)
+                
+                #delete self-edge
+                if edge.id1 == edge.id2:
+                    to_remove.append(edge)
+
+            for edge in to_remove:        
+                self.remove_edge(edge)
+
+
 	
 	def __str__(self):
+            string = ""
+            for vertex in self.cVertices:
+                string = string + str(vertex) + "\n"
+
+            return string
 		
 			
 
@@ -84,6 +174,19 @@ def build_graph_from_file(filename):
 
 	return graph
 
+
 graph = build_graph_from_file("kargerMinCut.txt")
 
-print graph
+
+while graph.num_vertices > 2:
+    graph.random_contract()
+#    print "num vertices left = " + str(graph.num_vertices)
+
+print "min_cut = " + str(graph.num_edges)
+
+
+
+
+
+
+
